194b2f0bc1114911830dffd4ababc202db5cab96
brennankbrown <mail@brennanbrown.ca>
2026-05-25 12:10:03 +1000

    chore: update formula to v1.1.2

--- Formula/writer.rb
+++ Formula/writer.rb
@@ -1,8 +1,8 @@
 class Writer < Formula
   desc "CLI post creation tool for static site generator blogs"
   homepage "https://github.com/brennanbrown/writer-cli"
-  url "https://github.com/brennanbrown/writer-cli/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "b758352dbd37811c6ef6b80689dab3b81f4ee4dc082bde8294541310e6e24bdf"
+  url "https://github.com/brennanbrown/writer-cli/archive/refs/tags/v1.1.2.tar.gz"
+  sha256 "845e16dbf3c438196754d9a6956db265e5f371b5eb061531ca9b54670f2a5096"
   license "AGPL-3.0-only"
 
   depends_on "bash"
